8473387b1c3d7f1167d95fc4cf442c4f8a970d9e
SWAT Nguyen Hoai Nam <namnhcntt@gmail.com>
2025-08-08 10:48:19 +0700

    feat(db): add SoKyHieu field to Document entity and update configurations
    - Add SoKyHieu field with max length 200 to Document entity
    - Update appsettings.json with new BaseDirectory path
    - Add Microsoft.EntityFrameworkCore.Design package to API and Infrastructure projects
    - Update launchSettings.json with new applicationUrl
    - Add initial database migrations

---
+++ BE/Admin/Infrastructure/Migrations/20250808034619_AddSoKyHieu.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoKyHieu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SoKyHieu",
+                table: "Document",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SoKyHieu",
+                table: "Document");
+        }
+    }
+}
